a9130a247f4d73e11d7674ae8d08c314369cd12f
Pil Ho Kim <pilhokim@gmail.com>
2014-12-23 02:20:14 +0100

    Clean source
    Clean source

--- api/media/SensorCEPManager.js
+++ api/media/SensorCEPManager.js
@@ -72,82 +72,6 @@ Ext.define('Elog.api.media.SensorCEPManager', {
     	});
     },
     
-    
-    
-    /**
-     * Search Sensor key value data
-     * 
-     * @param {Object} cfg
-     * @param {Object} sensors sensor names list
-     * @param {String} cfg.timeFrom Start time to search in unix timestamp in UTC
-     * @param {String} cfg.timeTo End time to search in unix timestamp in UTC
-     */
-    /*
-    getSensorDatabyTimeSpan: function (cfg) {
-    	var oSensorKeyValueManager = this;
-    	this.getServerQuery({
-    		command: this.getCommands().getSensorData,
-    		params: cfg,
-    		onSuccess: function(oResult) {
-    			if (typeof oResult.root == "undefined") {
-    				oSensorKeyValueManager.logError('Server connection failed. Check the internet connection');
-                	if (typeof cfg.onFail !== "undefined") {
-            			cfg.onFail(oResult);
-            		}
-                	return false;
-    			}
-    			else {
-    				if (Array.isArray(oResult.root) == false) {
-    					if (oResult.root == "false") return false;
-    					else return oResult.root;
-    				}
-    				else {
-	    				oResult.root.forEach(function(oData, i) {
-							if (typeof oData.data !== 'undefined') {
-								var oString = oData.data.replace(/"/g,"'").replace(/\n/g,"");
-								var oDataObject = Ext.JSON.decode(oString, true);
-								
-								if (oDataObject != null && oDataObject.hasOwnProperty("eml_event_timestamp")) {
-									if (oDataObject.hasOwnProperty("newEvent") &&oDataObject.newEvent != null) { // Unlike oldEvent, newEvent property is always set but it could be null, July 1, 2014
-										oDataObject.newEvent = oSensorKeyValueManager.getEventData(oDataObject.newEvent);
-										
-										//	oDataObject.newEvent.eml_event_timestamp = oDataObject.eml_event_timestamp;
-										if (oDataObject.newEvent != null && oDataObject.hasOwnProperty("eml_event_timestamp") && oDataObject.eml_event_timestamp != null) {
-											oDataObject.newEvent["eml_event_timestamp"] = oDataObject.eml_event_timestamp;
-										}
-									}
-									
-									if (typeof(oDataObject.oldEvent) != "undefined") oDataObject.oldEvent = oSensorKeyValueManager.getEventData(oDataObject.oldEvent);
-									
-									if (typeof(oDataObject.reportStatus) != "undefined") oDataObject.reportStatus = oSensorKeyValueManager.base64Decode(oDataObject.reportStatus.replace(/\n/g,""));
-									if (typeof(oDataObject.status) != "undefined") oDataObject.status = oSensorKeyValueManager.base64Decode(oDataObject.status.replace(/\n/g,""));
-									
-									// Set data icon
-									if (i == 0) oDataObject.iconDiv = "elogSKVIconStart";
-									else if (i == oResult.root.length -1) oDataObject.iconDiv = "elogSKVIconEnd";
-									else oDataObject.iconDiv = "elogSKVIcon";
-									
-									oData.data = oDataObject;						
-								}
-			    			}
-						});
-    				}
-    			}
-    			                
-            	if (typeof cfg.onSuccess !== "undefined") {
-        			cfg.onSuccess(oResult);
-        		}
-            },            
-            onFail: function(oResult) {
-            	oSensorKeyValueManager.logError('Server connection failed. Check the internet connection');
-            	if (typeof cfg.onFail !== "undefined") {
-        			cfg.onFail(oResult);
-        		}
-            }
-    	});
-    },
-    */
-    
     /**
      * Search Sensor key value data
      * 
@@ -203,7 +127,6 @@ Ext.define('Elog.api.media.SensorCEPManager', {
             }
     	});
     },
-    
     
     /**
      * Search Sensor key value data
